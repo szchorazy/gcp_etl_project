fae6d41a3022f8bf0e3d31c7bd2154d52ec90656
szchorazy <138896264+szchorazy@users.noreply.github.com>
2025-03-05 21:44:56 +0100

    Update load.py

--- mage/load.py
+++ mage/load.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.bigquery import BigQuery
 from mage_ai.io.config import ConfigFileLoader
